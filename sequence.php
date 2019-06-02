88ae6d0419e07b661443d9c822436831114daf28
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-06-03 00:20:03 +0200

    Clean up

--- sequence.php
+++ sequence.php
@@ -5,9 +5,6 @@ include_once __DIR__ . '/vendor/autoload.php';
 
 use steinmb\scanner;
 
-//$directoryToScan = __DIR__ . '/../PLAYLIST/';
-//$sequence = ['00506', '00502', '00516', '00515', '00510', '00500', '00513', '00508'];
-
 if (count($argv) !== 3) {
     echo 'Usage: php sequence.php <directory to scan> <comma separated list of mpls tracks>' . PHP_EOL;
     echo 'Example: php sequence.php /Users/steinmb/PLAYLIST "00506, 00500, 00001, 00502"' . PHP_EOL;
